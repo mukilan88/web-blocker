99079498ddf4722c9a9a3ce72f04870497432126
Mukilan <mukilan88@gmail.com>
2019-09-25 07:32:42 +0530

    master

---
+++ Readme.md
@@ -0,0 +1,30 @@
+
+# web-blocker App
+
+This is a python based web blocker app which will block the website you dont want to use in working hours.
+
+## Prerequisites
+
+We need to know where is the hosts file in the system
+for mac it is in the etc/hosts
+for windows it is in the C: drive  
+
+# Running the tests
+
+Run the code in the terminal as python3 filename.py after that clear the hosts file in the system to open the web-pages. 
+
+# License
+
+    Copyright 2019 Mukilan Selvaraj
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ website-blocker.py
@@ -0,0 +1,34 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp="hosts"
+# for windows computer use this hosts_path 
+# this is temp file path for the system
+# we can use exact file from c:\windos\system3\drivers\etc\hosts
+hosts_path=r"D:\Dropbox\pp\block_websites\Demo\hosts"
+# for mac linux computer use this hosts_path
+hosts_path="/etc/hosts"
+redirect="127.0.0.1"
+# add list of block_websites
+website_list=["www.facebook.com","facebook.com","gmail.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...")
+    time.sleep(5)
